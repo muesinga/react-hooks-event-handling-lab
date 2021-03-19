6bcf36a5c80de195280b99adf177790a5e54f652
“Aaron <github email address>
2021-03-19 13:24:33 -0400

    completed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
-// Code Keypad Component Here
+function Keypad() {
+
+    function enterPassword() {
+        console.log('Entering password...')
+    }
+
+    return (
+        <div>
+            <input onChange={enterPassword} type="password" /> 
+        </div>
+    )
+}
+
+export default Keypad;
